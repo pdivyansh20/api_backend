d238c65b6903256d38c0c999fa77b8b87e43d66b
pdivyansh20 <84575211+pdivyansh20@users.noreply.github.com>
2022-05-15 11:17:38 +0530

    make a branch which will directly show data

--- api.js
+++ api.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const https = require("https")
-const bodyParser = require("body-parser")
 const app = express()
-app.use(bodyParser.urlencoded({extended:true}))
 app.get("/", function(req,res) {
-    res.sendFile(__dirname+"/index.html")
-})
- 
-app.post("/",function(req,res){ 
-   const query= req.body.cityName
-   const apiKey= "0c31c634f1c4e56047eb21ed8c92adbe"
+    const query= "seoul"
+    const apiKey= "0c31c634f1c4e56047eb21ed8c92adbe"
    const url = "https://api.openweathermap.org/data/2.5/weather?appid="+apiKey+"&q="+query;
    https.get(url,function(response){
        response.on("data",function(data){
@@ -23,6 +17,7 @@ app.post("/",function(req,res){
        })
    })
 })
+ 
 
 
 app.listen(4000, function(){
