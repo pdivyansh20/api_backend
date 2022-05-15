be7dc26fc880e18eccc2484ea44431c683c41bd2
pdivyansh20 <84575211+pdivyansh20@users.noreply.github.com>
2022-05-15 11:11:13 +0530

    create master branch which takes user input

---
+++ api.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const https = require("https")
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/", function(req,res) {
+    res.sendFile(__dirname+"/index.html")
+})
+ 
+app.post("/",function(req,res){ 
+   const query= req.body.cityName
+   const apiKey= "0c31c634f1c4e56047eb21ed8c92adbe"
+   const url = "https://api.openweathermap.org/data/2.5/weather?appid="+apiKey+"&q="+query;
+   https.get(url,function(response){
+       response.on("data",function(data){
+           const weatherData = JSON.parse(data) ;
+           const temp = weatherData.main.temp ;
+           const icon= weatherData.weather[0].icon;
+           const imageUrl= "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+           res.write(" <h1> Temp in "+query+" is "+ temp +"<h1/>")  
+           res.write("<img src="+imageUrl+">")
+           res.send()
+       })
+   })
+})
+
+
+app.listen(4000, function(){
+    console.log("server is running")
+})
